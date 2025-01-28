624ec59f59c8b433768e5cdff960e0bb5894552a
H4ckint0sh <alijan.adeli@icloud.com>
2025-01-28 23:19:49 +0100

    feat: no caching

--- lua/nord/plugins/snacks.lua
+++ lua/nord/plugins/snacks.lua
@@ -5,12 +5,14 @@ local c = require("nord.colors").palette
 function snacks.highlights()
   return {
     SnacksDashboardDir = { link = "Comment" },
-
     SnacksPickerTotals = { link = "Comment" },
-    SnacksPickerDir = { link = "Comment" },
+    SnacksPickerDir = { link = "Normal" },
     SnacksPickerBufFlags = { link = "Comment" },
     SnacksPickerKeymapRhs = { link = "Comment" },
     SnacksPickerGitStatus = { link = "Comment" },
+    SnacksIndent = { link = "Normal" },
+    SnacksPickerCol = { link = "Normal" },
+    SnacksPickerIndent = { link = "Normal" },
   }
 end
 
