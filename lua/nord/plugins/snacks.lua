f366a55d4f33f94c60616177bbc65c694e165e78
H4ckint0sh <alijan.adeli@icloud.com>
2025-09-25 18:49:43 +0200

    fix: snacks

--- lua/nord/plugins/snacks.lua
+++ lua/nord/plugins/snacks.lua
@@ -1,15 +1,20 @@
 local snacks = {}
 
+local c = require("nord.colors").palette
+
 function snacks.highlights()
   return {
     SnacksDashboardDir = { link = "Comment" },
+
     SnacksPickerTotals = { link = "Comment" },
+    SnacksPickerDir = { link = "Normal" },
     SnacksPickerBufFlags = { link = "Comment" },
     SnacksPickerKeymapRhs = { link = "Comment" },
     SnacksPickerGitStatus = { link = "Comment" },
-    SnacksPickerDir = { link = "Text" },
-    SnacksPickerPathHidden = { link = "Text" },
-    SnacksPickerGitStatusUntracked = { link = "Special" },
+    SnacksPickerPathHidden = { fg = c.snow_storm.origin },
+    SnacksPickerPathIgnored = { fg = c.snow_storm.origin },
+    SnacksPickerGitStatusIgnored = { fg = c.snow_storm.origin },
+    SnacksPickerGitStatusUntracked = { fg = c.snow_storm.origin },
   }
 end
 
