a9d5874518fe339548358e5abe0e73d5d0d15a41
H4ckint0sh <alijan.adeli@icloud.com>
2025-09-25 18:18:32 +0200

    fix: Snacks explorer hidden directories

--- lua/nord/plugins/snacks.lua
+++ lua/nord/plugins/snacks.lua
@@ -1,15 +1,15 @@
 local snacks = {}
 
-local c = require("nord.colors").palette
-
 function snacks.highlights()
   return {
     SnacksDashboardDir = { link = "Comment" },
     SnacksPickerTotals = { link = "Comment" },
-    SnacksPickerDir = { link = "Comment" },
     SnacksPickerBufFlags = { link = "Comment" },
     SnacksPickerKeymapRhs = { link = "Comment" },
     SnacksPickerGitStatus = { link = "Comment" },
+    SnacksPickerDir = { link = "Text" },
+    SnacksPickerPathHidden = { link = "Text" },
+    SnacksPickerGitStatusUntracked = { link = "Special" },
   }
 end
 
